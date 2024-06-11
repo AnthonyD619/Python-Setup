ae700afdc783bad0331e4c5ecfd6970fd8cf299d
“Anthony <anthony.deguire619@gmail.com>
2024-06-11 12:52:24 -0700

    first commit

---
+++ functions_practice.py
@@ -0,0 +1,16 @@
+def Hello():
+  print("Hello, my name is Anthony!")
+
+def pack(anthony, michael, eric):
+  
+  return [anthony, michael, eric]
+
+def eat_lunch(my_lst):
+  if len(my_lst) == 0:
+    print("My lunchbox is empty!")
+  else:
+    for i in range(len(my_lst)):
+      if i == 0:
+        print(f"First I eat {my_lst[0]}")
+      else:
+        print(f"Next I eat {my_lst[i]}")
